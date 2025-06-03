a252ed0bb88ba6bb9162c6e8de1cf81f13f25793
JK <kagarukijackson5@gmail.com>
2025-06-03 09:07:48 +0300

    Update problemRoutes.js

--- routes/problemRoutes.js
+++ routes/problemRoutes.js
@@ -4,9 +4,9 @@ const problemController = require('../controllers/problemController');
 
 // Problem routes
 router.get('/', problemController.getAllProblems);
+router.get('/stats', problemController.getProblemStats);  // Moved here before /:id
 router.get('/:id', problemController.getProblemById);
 router.post('/', problemController.createProblem);
 router.put('/:id/status', problemController.updateProblemStatus);
-router.get('/stats', problemController.getProblemStats);
 
 module.exports = router;
